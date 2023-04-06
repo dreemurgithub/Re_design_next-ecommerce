04a8409e6f42ece309805bee6c38d6b2c11f72db
dreemurgithub <96066405+dreemurgithub@users.noreply.github.com>
2023-04-07 04:34:40 +0700

    add search string to api/post/[id]

---
+++ src/pages/api/products/index.ts
@@ -0,0 +1,10 @@
+import type {NextApiRequest,NextApiResponse} from "next";
+import {promises as fs} from 'fs';
+import path from 'path';
+// import fs from 'fs';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse){
+    const filepath = path.join(process.cwd(),'file','post.json')
+    const data = await fs.readFile(filepath,'utf8')
+    res.status(200).send(data)
+}
