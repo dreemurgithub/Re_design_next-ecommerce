f5acaa4539f53dcfbbbaee1b957f110519a01ba1
dreemurgithub <96066405+dreemurgithub@users.noreply.github.com>
2023-04-08 02:08:37 +0700

    create simple store and cart, can + - every item, havent update by string(action.payload keep display the current state)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,8 @@
-// import '@/styles/globals.css'
-// import {Provider} from 'react-redux'
-// import {store} from "../store";
 import type {AppProps} from 'next/app'
 import {Provider} from "react-redux";
 import {store} from '../store'
 export default function App({Component, pageProps}: AppProps) {
+    // console.log(store.reducer)
     return <Provider store={store}>
 
         <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import * as dotenv from 'dotenv'
 import {useEffect, useState} from "react"; // work
+import {useStore,useSelector,useDispatch} from "react-redux";
+import {add_amount,test,CartState} from "@/store";
 
 export default function Home() {
     const [post_list,setlist]=useState([])
+
+    const cart_state = useSelector((state : {cart:CartState }) =>state)
+
+    const a_state = useSelector((state : {cart:CartState })=>state.cart.a)
+    const dispatch = useDispatch()
     useEffect(()=>{
         fetch('/api/products')
             .then(res=>res.json())
@@ -11,12 +18,23 @@ export default function Home() {
     return <>
             <main>
                 <h1>Hello world</h1>
+                <h2>{JSON.stringify(cart_state)}</h2>
+                <h3>{a_state}</h3>
                 {post_list.map((el : any )=> <>
                         <h3>{el.id}</h3>
                     <img src={el.url} alt="" width={144} height={144}/>
                     <p>{el.content}</p>
-                    <button>Add to cart</button>
-                    </>)}
+
+                    <button onClick={()=> {
+                        // dispatch(add_amount({payload: el.id} ) )
+                    }}>Add to cart</button>
+
+                    <button onClick={()=> {
+                        dispatch(test(cart_state.cart))
+                        // dispatch(test(cart_state.cart))
+                    }}>test</button>
+
+                </>)}
             </main>
         </>
 }
